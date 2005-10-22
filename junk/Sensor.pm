f80f56068be8677eeb42e10bf0eab0b2af9efdba
robertn <robertn@ea033599-1a03-0410-9129-b9884834fa95>
2005-10-22 22:11:43 +0000

    Sensor class

--- junk/Sensor.pm
+++ junk/Sensor.pm
@@ -1,27 +1,63 @@
 #!/usr/bin/perl
 
 package Sensor;
+
+use Storable 'nstore_fd';
+
 require Exporter;
 @ISA = qw(Exporter);
+# (nie wolno eksportowac metod)
 
+#
+# Sensor powinien miec atrybuty
+# 	nazwa
+# 	deskryptor gniazda
+#
 
 sub new($) {
 	print "konstruktor\n";
 	my $self = {
-		'name' => $_[0]
+		'name' => $_[0],
+		'socket' => \*STDOUT,
 	};
 	return bless $self;
 }
 
 sub info() {
-	print "Jestem sensor\n";
+	print "Jestem sensor ${\($_[0]->{name})}\n";
 }
 
-
+#
+# XXX
+# Akcesory i modifykatory powinny byæ robione automatycznie
+# [automatyczna modifikacja wpisów w przestrzeni nazw modu³u]
+#
 sub getName() {
 	return shift->{'name'};
 }
 
+sub AUTOLOAD {
+	print "Powinienem sprobowac wywolac zdalnie $AUTOLOAD:\n";
+	shift->call($AUTOLOAD, defined wantarray, @_);
+}
+
+
+#
+# Wywo³uje zdalnie na tym sensorze podan± funkcjê
+#
+sub call {
+	my ($self, $name, $arrayctx, @args) = @_;
+	my $sensor = $self->{name};
+	local $" = ',';
+	print "Wywo³ujê zdalnie na sensorze $sensor funkcje $name(@args) w kontekscie "
+	. ($arrayctx ? 'listowym' : 'skalarnym') . "\n";
+
+	nstore_fd([$name, $arrayctx, @args], $self->{socket});
+}
+
+sub DESTROY {
+	print "Destruktor Sensora ${\($_[0]->{'name'})}\n";
+}
 
 1;
 
--- junk/main.pl
+++ junk/main.pl
@@ -6,5 +6,15 @@ use Sensor;
 
 my $s1 = Sensor::new('sensor1');
 
-print $s1->getName();
+print $s1->getName() . "\n";
+
+
+my %sensors;
+$sensors{$s1->getName()} = $s1;
+
+
+$sensors{'sensor1'}->setFingerprint('192.168.15.19', 'Tru64');
+
+
+print "\n";
 
