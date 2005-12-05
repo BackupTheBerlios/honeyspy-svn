c717604800b8710f1c09c1424795482e84d29d53
robertn <robertn@ea033599-1a03-0410-9129-b9884834fa95>
2005-12-05 01:25:53 +0000

    docs update

--- junk/p0f.pl
+++ junk/p0f.pl
@@ -7,12 +7,14 @@ use strict;
 
 use Net::P0f;
 
+close STDERR;
+
 $| = 1;
 
 my $p0f = Net::P0f->new(
 	interface => 'any',
 	promiscuous => 1,
-	detection_mode => 1,
+	detection_mode => 0,
 #	fuzzy => 1,
 	promiscuous => 1,
 #	masquerade_detection => 1
@@ -28,16 +30,17 @@ $SIG{INT} = sub {
 for (;;) {
 	print "Starting...\n";
 	$p0f->loop(
-		count => 1,
+		count => 1000,
 		callback => \&process_packet,
 	);
-	sleep 3;
 }
 
 sub process_packet  {
 	my ($self, $header, $os_info, $link_info) = @_;
 
-	print "OS on $header->{ip_src} recognized: ";
+	return if $os_info->{genre} eq 'UNKNOWN';
+
+	print "$header->{ip_src} is probably running: ";
 	print "$os_info->{genre} ($os_info->{details}), ";
 	print "uptime: $os_info->{uptime} hours (?)\n";
 }
